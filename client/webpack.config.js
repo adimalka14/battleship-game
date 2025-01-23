88aaa8dfb6609eb6a062cb00cbe37a1923bb36df
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2025-01-23 16:32:35 +0200

    fix bug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -25,4 +26,9 @@ module.exports = {
         open: true,
         hot: true,
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.SERVER_URL': JSON.stringify(process.env.SERVER_URL || 'http://localhost:3000'),
+        }),
+    ],
 };
